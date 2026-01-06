f0776b46d81d9ffe3efcdb69294c9eb28b1f5a5f
L4rralde <larraldeortizemmanuelalejandro@gmail.com>
2026-01-06 09:42:11 -0600

    Using KittiOdom class as dataloader

--- poseformer/training/datasets/kitti_odom.py
+++ poseformer/training/datasets/kitti_odom.py
@@ -1,12 +1,21 @@
 import os
 from glob import glob
-from typing import Callable, List, Dict, Any
+from typing import List, Any, Iterable
+import random
+from dataclasses import dataclass, fields
 
 import numpy as np
-from torch.utils.data import Dataset
+from PIL import Image
+
 
 #Sequence lenght?
 #Different sequences. Different sizes.
+
+def split_list_complete_chunks(x: list, chunk_size: int) -> List[List[Any]]:
+    chunks = [x[i: i+chunk_size] for i in range(0, len(x), chunk_size)]
+    if len(chunks[-1]) < chunk_size:
+        chunks = chunks[:-1]
+    return chunks
 
 
 def read_pose_txt(path: str) -> List[np.ndarray]:
@@ -21,14 +30,19 @@ def read_times_file(path: str) -> List[int]:
     return times.tolist()
 
 
-def split_list_complete_chunks(x: list, chunk_size: int) -> List[list]:
-    chunks = [x[i: i+chunk_size] for i in range(0, len(x), chunk_size)]
-    if len(chunks[-1]) < chunk_size:
-        chunks = chunks[:-1]
-    for chunk in chunks:
-        if len(chunk) != chunk_size:
-            raise RuntimeError("Incorrect implementation")
-    return chunks
+@dataclass
+class View:
+    path: str
+    pose: np.ndarray
+    time_stamp: float
+    source: str
+
+    @property
+    def image(self) -> Image.Any:
+        return Image.open(self.path)
+
+    def __repr__(self) -> str:
+        return f"View({self.source}, stamp={self.time_stamp:.2f})"
 
 
 class KittiOdomSequence:
@@ -46,43 +60,73 @@ class KittiOdomSequence:
         img_paths = [str(os.path.join(imgs_dir_path, path)) for path in img_paths]
         assert len(poses) == len(times) == len(img_paths)
 
-        self.data = list(zip(img_paths, poses, times, [self.key for _ in range(len(poses))]))
-        self.data.sort(key = lambda x: x[2])
+        self.data = [
+            View(path, pose, stamp, f"kitti_odom_{self.key}")
+            for path, pose, stamp in zip(img_paths, poses, times)
+        ]
+        self.data.sort(key = lambda x: x.time_stamp)
 
     def __len__(self) -> int:
         return len(self.data)
 
-    def __getitem__(self, i: int) -> tuple:
+    def __getitem__(self, i: int) -> View:
         return self.data[i]
 
-    def tolist(self) -> List[tuple]:
+    def tolist(self) -> List[View]:
         return list(self.data)
 
 
-class KittiOdomDataset(Dataset):
-    def __init__(self, data_root: str, seq_len: int=24, img_transform: Callable|None=None):
+class KittiOdom:
+    def __init__(
+        self,
+        data_root: str,
+        seq_len: int=24,
+    ):
         self.data_root = data_root
-        self.img_transform = img_transform
-        self.seq_len = seq_len
         poses_path = os.path.join(data_root, "poses")
         available_posed_seqs = [
             os.path.splitext(f)[0]
             for f in glob("*.txt", root_dir=poses_path)
         ]
 
-        self.data = []
+        self.kitty_odom_sequences = []
         for seq in available_posed_seqs:
             try:
                 kitti_posed_sequence = KittiOdomSequence(data_root, seq)
-                chunks = split_list_complete_chunks(kitti_posed_sequence.tolist(), seq_len)
-                self.data += chunks
+                self.kitty_odom_sequences.append(kitti_posed_sequence)
             except Exception as e:
-                print(e)
                 continue
 
+        self.sequences = self.chunk_sequences(seq_len)
 
     def __len__(self) -> int:
-        return len(self.data)
+        return len(self.sequences)
 
-    def __getitem__(self, i: int) -> "KittiOdomSequence":
-        return self.data[i]
+    def __getitem__(self, i: int) -> List[View]:
+        return self.sequences[i]
+
+    def chunk_sequences(self, seq_len: int) -> List[List[View]]:
+        chunks = []
+        for seq in self.kitty_odom_sequences:
+            chunks += split_list_complete_chunks(seq.tolist(), seq_len)
+        return chunks
+
+    def reset_seq_len(self, seq_len: int) -> None:
+        self.sequences = self.chunk_sequences(seq_len)
+
+    def shuffle(self) -> None:
+        random.shuffle(self.sequences)
+
+    def get_batches(self, *,batch_size: int, sample_size: int) -> Iterable:
+        for batch_i in range(0, self.__len__(), batch_size):
+            batch = self.sequences[batch_i: batch_i+batch_size]
+            batch = [random.sample(seq, sample_size) for seq in batch]
+            attributes = ['image'] + [f.name for f in fields(View)]
+            yield {
+                att: [
+                    [view.__getattribute__(att) for view in seq]
+                    for seq in batch
+                ]
+                for att in attributes
+            }
+        
